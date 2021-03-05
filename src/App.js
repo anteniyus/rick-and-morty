ee10ba4f040b74a997de90e29cbc6887cb84a73d
Reza <development.rezasajadi@gmail.com>
2021-03-05 23:01:17 +0330

    Adding routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import RickAndMortyStore from "./dataStore/DataStore";
 import Home from "./screens/home/Home";
@@ -7,7 +13,12 @@ import Home from "./screens/home/Home";
 function App() {
   return (
     <Provider store={RickAndMortyStore}>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/character" component={Home} />
+          <Redirect from="/" to="/character" exact />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
