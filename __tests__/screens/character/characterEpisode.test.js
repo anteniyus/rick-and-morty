3057f56ba19c4b01525e241d886d30c5ce58f6c1
Reza <development.rezasajadi@gmail.com>
2021-03-06 18:52:55 +0330

    Adding tests for characterEpisode.

---
+++ __tests__/screens/character/characterEpisode.test.js
@@ -0,0 +1,131 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import { Typography } from "@material-ui/core";
+
+import CharacterEpisode from "../../../src/screens/character/profile/CharacterEpisode";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let episodes = [
+  {
+    id: 1,
+    name: "Pilot",
+    air_date: "December 2, 2013",
+    episode: "S01E01",
+    characters: [
+      "https://rickandmortyapi.com/api/character/1",
+      "https://rickandmortyapi.com/api/character/2",
+      "https://rickandmortyapi.com/api/character/35",
+      "https://rickandmortyapi.com/api/character/38",
+      "https://rickandmortyapi.com/api/character/62",
+      "https://rickandmortyapi.com/api/character/92",
+      "https://rickandmortyapi.com/api/character/127",
+      "https://rickandmortyapi.com/api/character/144",
+      "https://rickandmortyapi.com/api/character/158",
+      "https://rickandmortyapi.com/api/character/175",
+      "https://rickandmortyapi.com/api/character/179",
+      "https://rickandmortyapi.com/api/character/181",
+      "https://rickandmortyapi.com/api/character/239",
+      "https://rickandmortyapi.com/api/character/249",
+      "https://rickandmortyapi.com/api/character/271",
+      "https://rickandmortyapi.com/api/character/338",
+      "https://rickandmortyapi.com/api/character/394",
+      "https://rickandmortyapi.com/api/character/395",
+      "https://rickandmortyapi.com/api/character/435",
+    ],
+    url: "https://rickandmortyapi.com/api/episode/1",
+    created: "2017-11-10T12:56:33.798Z",
+  },
+  {
+    id: 2,
+    name: "Pilot",
+    air_date: "December 2, 2013",
+    episode: "S01E01",
+    characters: [
+      "https://rickandmortyapi.com/api/character/1",
+      "https://rickandmortyapi.com/api/character/2",
+      "https://rickandmortyapi.com/api/character/35",
+      "https://rickandmortyapi.com/api/character/38",
+      "https://rickandmortyapi.com/api/character/62",
+      "https://rickandmortyapi.com/api/character/92",
+      "https://rickandmortyapi.com/api/character/127",
+      "https://rickandmortyapi.com/api/character/144",
+      "https://rickandmortyapi.com/api/character/158",
+      "https://rickandmortyapi.com/api/character/175",
+      "https://rickandmortyapi.com/api/character/179",
+      "https://rickandmortyapi.com/api/character/181",
+      "https://rickandmortyapi.com/api/character/239",
+      "https://rickandmortyapi.com/api/character/249",
+      "https://rickandmortyapi.com/api/character/271",
+      "https://rickandmortyapi.com/api/character/338",
+      "https://rickandmortyapi.com/api/character/394",
+      "https://rickandmortyapi.com/api/character/395",
+      "https://rickandmortyapi.com/api/character/435",
+    ],
+    url: "https://rickandmortyapi.com/api/episode/1",
+    created: "2017-11-10T12:56:33.798Z",
+  },
+];
+
+describe("<CharacterLocationOriginInfo />", () => {
+  beforeEach(() => {
+    jest.spyOn(console, "error");
+    jest.spyOn(console, "warn");
+  });
+
+  afterEach(() => {
+    /* eslint-disable no-console,jest/no-standalone-expect */
+    expect(console.error).not.toBeCalled();
+    expect(console.warn).not.toBeCalled();
+  });
+
+  describe("props", () => {
+    test("check props requirements", () => {
+      episodes = [];
+
+      const wrapper = shallow(<CharacterEpisode episodes={episodes} />);
+
+      expect(wrapper.instance().props.episodes).not.toBe(undefined);
+    });
+  });
+
+  describe("render()", () => {
+    test("render the component with no data", () => {
+      episodes = [];
+
+      const wrapper = shallow(<CharacterEpisode episodes={episodes} />);
+
+      const typographyCount = wrapper.find(Typography).length;
+
+      expect(typographyCount).toBe(1);
+    });
+  });
+
+  describe("render()", () => {
+    test("check state", () => {
+      const wrapper = shallow(<CharacterEpisode episodes={episodes} />);
+
+      const episodesName = episodes.map((episode) => episode.name);
+      wrapper.setState({ episodesName });
+
+      const stateData = wrapper.state();
+
+      expect(stateData.episodesName).toBe(episodesName);
+    });
+  });
+
+  describe("render()", () => {
+    test("renders the component with data", () => {
+      const wrapper = shallow(<CharacterEpisode episodes={episodes} />);
+
+      const episodesName = episodes.map((episode) => episode.name);
+      wrapper.setState({ episodesName });
+
+      const typographyCount = wrapper.find(Typography).length;
+
+      expect(typographyCount).toBe(episodesName.length + 1);
+    });
+  });
+});
